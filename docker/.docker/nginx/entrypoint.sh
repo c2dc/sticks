0bf8666184058d09b18c68014dc37c968ba68a05
khaosBRA <agneyroth@gmail.com>
2026-03-16 20:12:03 -0300

    fix on entrypoint

--- docker/.docker/nginx/entrypoint.sh
+++ docker/.docker/nginx/entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 echo "[*] Starting services..."
-php-fpm8.4 -D
-nginx
-
+pgrep -x "php-fpm8.4" > /dev/null || php-fpm8.4 &
+pgrep -x "nginx" > /dev/null || /usr/sbin/nginx &
 echo "[*] Initializing campaign environments..."
 
-/apt41_dust_suta.sh
-/c0010_suta.sh
-/c0026_suta.sh
-/costaricto_suta.sh
-/operation_midnighteclipse_suta.sh
-/outer_space_suta.sh
-/salesforce_data_exfiltration_suta.sh
-/shadowray_suta.sh
+/apt41_dust_suta.sh &
+/c0010_suta.sh &
+/c0026_suta.sh &
+/costaricto_suta.sh &
+/operation_midnighteclipse_suta.sh &
+/outer_space_suta.sh & 
+/salesforce_data_exfiltration_suta.sh &
+/shadowray_suta.sh &
 
 echo "[*] Environment ready."
 
-nginx -g "daemon off;"
